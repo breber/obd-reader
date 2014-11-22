db00a093d6f77dfd8e8c698f4ce00dccb1d93a1c
Brian Reber <reber.brian@gmail.com>
2014-11-21 19:16:04 -0600

    Add basic integration of OBD api

--- app/src/main/java/com/brianreber/obdreader/MainActivity.java
+++ app/src/main/java/com/brianreber/obdreader/MainActivity.java
@@ -1,19 +1,61 @@
 package com.brianreber.obdreader;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends Activity {
+
+    private Thread runThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        ArrayList<String> deviceStrs = new ArrayList<String>();
+        final ArrayList<String> deviceAddresses = new ArrayList<String>();
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (!pairedDevices.isEmpty()) {
+            for (BluetoothDevice device : pairedDevices) {
+                deviceStrs.add(device.getName() + "\n" + device.getAddress());
+                deviceAddresses.add(device.getAddress());
+            }
+        }
+
+        // show list
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice,
+                deviceStrs.toArray(new String[deviceStrs.size()]));
+
+        alertDialog.setSingleChoiceItems(adapter, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                int position = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+                String deviceAddress = deviceAddresses.get(position);
+
+                // Start the thread that reads the data
+                runThread = new Thread(new RunThread(MainActivity.this, deviceAddress));
+                runThread.start();
+            }
+        });
+
+        alertDialog.setTitle("Choose Bluetooth device");
+        alertDialog.show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/brianreber/obdreader/RunThread.java
@@ -0,0 +1,81 @@
+package com.brianreber.obdreader;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.UUID;
+
+import pt.lighthouselabs.obd.commands.engine.EngineRuntimeObdCommand;
+import pt.lighthouselabs.obd.commands.protocol.EchoOffObdCommand;
+import pt.lighthouselabs.obd.commands.protocol.LineFeedOffObdCommand;
+import pt.lighthouselabs.obd.commands.protocol.SelectProtocolObdCommand;
+import pt.lighthouselabs.obd.commands.protocol.TimeoutObdCommand;
+import pt.lighthouselabs.obd.enums.ObdProtocols;
+
+/**
+ * Created by breber on 11/21/14.
+ */
+public class RunThread implements Runnable {
+
+    private static final String TAG = "RunThread";
+    private String mDeviceAddress;
+    private Activity mContext;
+
+    public RunThread(Activity activity, String deviceAddress) {
+        mContext = activity;
+        mDeviceAddress = deviceAddress;
+    }
+
+    // See http://en.wikipedia.org/wiki/OBD-II_PIDs#Standard_PIDs
+
+    @Override
+    public void run() {
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothDevice device = btAdapter.getRemoteDevice(mDeviceAddress);
+
+        // UUID for serial device
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+        try {
+            BluetoothSocket socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+            socket.connect();
+
+            // Do some basic initialization
+            new EchoOffObdCommand().run(socket.getInputStream(), socket.getOutputStream());
+            new LineFeedOffObdCommand().run(socket.getInputStream(), socket.getOutputStream());
+            new TimeoutObdCommand(30).run(socket.getInputStream(), socket.getOutputStream());
+            new SelectProtocolObdCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
+
+            final EngineRuntimeObdCommand engineRuntimeCommand = new EngineRuntimeObdCommand();
+            while (!Thread.currentThread().isInterrupted()) {
+                engineRuntimeCommand.run(socket.getInputStream(), socket.getOutputStream());
+
+                // TODO handle commands result
+                Log.d(TAG, "Runtime: " + engineRuntimeCommand.getFormattedResult());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView tv = (TextView) mContext.findViewById(R.id.main_text);
+                        tv.setText("Runtime: " + engineRuntimeCommand.getFormattedResult());
+                    }
+                });
+
+                Thread.sleep(5000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void runOnUiThread(Runnable runnable){
+        final Handler uiHandler = new Handler(Looper.getMainLooper());
+        uiHandler.post(runnable);
+    }
+}
